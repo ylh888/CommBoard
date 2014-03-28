8a3d2bd814781f69aa3f70557520cc88a78a3dbd
ylh <ylh@computer.org>
2014-03-28 15:12:54 -0400

    v.701

--- commboard.js
+++ commboard.js
@@ -15,7 +15,9 @@ var debugMessage = "(space)=click | s=select | p=pause on/off | v=voice over sim
     state = 0,
     buttonPresented = 0,
     debouncetime = 100,
-    selectPressed = 0;
+    selectPressed = 0,
+    inSetup = 1;
+
 // state:: 0=cycle row, 1=cycle column, 2= action on button (row,col)
 
 var buttonText = [{
@@ -37,9 +39,9 @@ var buttonText = [{
         "t": "Non",
         kind: "SayIt"
     },
-    
+
     // row 1
-        {
+    {
         "t": "e",
         kind: "Alpha"
     }, {
@@ -60,7 +62,7 @@ var buttonText = [{
     },
 
     // row 2
-        {
+    {
         "t": "i",
         kind: "Alpha"
     }, {
@@ -81,7 +83,7 @@ var buttonText = [{
     },
 
     // row 3
-        {
+    {
         "t": "o",
         kind: "Alpha"
     }, {
@@ -102,7 +104,7 @@ var buttonText = [{
     },
 
      // row 4
-        {
+    {
         "t": "u",
         kind: "Alpha"
     }, {
@@ -123,7 +125,7 @@ var buttonText = [{
     },
 
      // row 5
-        {
+    {
         "t": "espace",
         kind: "Subs",
         substitute: " "
@@ -145,16 +147,22 @@ var buttonText = [{
     },
 ];
 
+var dayOfWeek = ["Sunday", "Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"];
+
+var today = new Date();
+var theDay = dayOfWeek[today.getDay()];
 
 function startTime() {
-    var today = new Date();
+    today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
     // add a zero in front of numbers<10
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
-    document.getElementById('clientTxt').innerHTML = "[" + h + ":" + m + ":" + s + "] " + clientTxt;
+    document.getElementById('timeDisplay').innerHTML = theDay +
+        "<br>" + h + ":" + m + ":" + s ;
+document.getElementById('clientTxt').innerHTML = clientTxt;
     document.getElementById('debug').innerHTML = debugMessage;
 
     t = setTimeout(function () {
@@ -250,10 +258,12 @@ function pauseOnOff() {
     if (pauseState) {
         document.getElementById('pauseButton').value = "RUN";
         doVideoSetUp();
+
         //debugMessage = "Paused.";
     } else {
         document.getElementById('pauseButton').value = "PAUSE";
         endVideoSetUp();
+
         //debugMessage = "Running ...";
     }
 }
@@ -415,7 +425,7 @@ function checkState() {
     if (pauseState) { // paused
         setTimeout(function () {
             checkState()
-        }, 300);
+        }, 1000);
     } else {
         if (selectPressed == 0) { // no key activity - steady state
             if (state == 0) {
@@ -450,13 +460,10 @@ function checkState() {
 
         //setTimeout( function(){checkState()}, 2000);
     }
-
 }
 
 function setTable() {
-
     var t = "<div id='menuTable'>";
-
     for (var i = 0; i < nRows; i++) {
         t += "<div class='rowOff' id='row" + i + "'>";
 
@@ -473,16 +480,18 @@ function setTable() {
         t += "</div>";
     }
     t += "</div>";
-
-    //t+="<script>document.getElementById('btn" + highlightButton + "').focus();</script>";
-
     document.getElementById("content").innerHTML = t;
+
+    // doTabIndexOnce
+    // >>>>>>>>>>>>>>>> to implement - set tabindex=-1 in the rest of the nodes
+    for (var j = 0; j < nRows * nCols; j++) {
+        document.getElementById("btn" + j).setAttribute("tabindex", j + 1);
+    }
 }
 
 function getStarted() {
 
     setTable();
-    doTabIndexOnce();
 
     highlightRow = nRows - 1;
     pauseState = 1;
@@ -493,14 +502,6 @@ function getStarted() {
     document.getElementById("pauseButton").focus();
     document.getElementById("pauseButton").select();
 }
-
-function myDelay(del) {
-    return;
-
-    var untilwhen = Date.now() + del;
-    while (untilwhen > Date.now()) {}
-}
-
 
 function tmpplaymp3(i) {
     playmp3(i);
@@ -513,8 +514,12 @@ function tmpplaymp3(i) {
     }
 }
 
+function SetUp(){
+
+        doVideoSetUp();
+}
+
 function doVideoSetUp() {
-    return; //******
 
     var t = "";
     for (var j = 0; j < nRows * nCols; j++) {
@@ -524,24 +529,26 @@ function doVideoSetUp() {
             " ></input>";
     }
     document.getElementById("videoSetUp").innerHTML = t;
+    document.getElementById("tmpB0").focus();
 
     /*
-    for (var i=0; i<nRows*nCols; i++) {
-        (function () {
-            audioElement2 = document.createElement('audio');
-            audioElement2.setAttribute('src', 'img/fr_' + buttonText[i].t.replace(/ /g,"+") + '.mp3' );
-            audioElement2.controls = true;
-            audioElement2.loop = false;
-            audioElement2.load();
-            audioElement2.play();
-        }());
-       // setTimeout( function() { simulatedClick(document.getElementById("tmpB"+j),"click");}, 800);
-    }
-    */
+     for (var i=0; i<nRows*nCols; i++) {
+     (function () {
+     audioElement2 = document.createElement('audio');
+     audioElement2.setAttribute('src', 'img/fr_' + buttonText[i].t.replace(/ /g,"+") + '.mp3' );
+     audioElement2.controls = true;
+     audioElement2.loop = false;
+     audioElement2.load();
+     audioElement2.play();
+     }());
+     // setTimeout( function() { simulatedClick(document.getElementById("tmpB"+j),"click");}, 800);
+     }
+     */
 }
 
 function endVideoSetUp() {
     document.getElementById("videoSetUp").innerHTML = "";
+    document.getElementById("pauseButton").focus();
 }
 
 function doTabIndexOnce() {
